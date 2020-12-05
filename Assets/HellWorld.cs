259013421f115cd2dc3dd982beb18d30077919b5
popote <yvan.bladet@gmail.com>
2020-12-05 22:10:38 +0100

    AddProject

---
+++ Assets/HellWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ludiq;
+using Bolt;
+
+   
+public class HellWorld : MonoBehaviour
+{
+   
+   public void DoAction(string Message)
+   {
+      Debug.Log("HelloWorld");
+     Debug.Log(Message);
+   }
+
+   public string OutText ()
+   {
+       return "test";
+   }
+
+   public void OutMultipleText(out string text, out string text2 , out float Unnombre )
+   {
+       text = "bla1";
+       text2 = "bla2";
+       Unnombre = 3;
+   }
+}
